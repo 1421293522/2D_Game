31d60188d34021b3207103dfab175b935b153f8c
zhoushiyao02200059 <1164641634@qq.com>
2025-07-09 17:44:30 +0800

    fix scripts, add collide

---
+++ FinalGame/Assets/Scripts/Humanoid/Player/HeroCollide.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroCollide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject e = collision.gameObject;
+        if (e.tag == "EnemyBullet")
+        {
+            GetComponent<HumanoidStatus>().GetHurt(10);
+        }
+        else if (e.tag == "Wall")
+        {
+            Debug.Log("Ouch!!!");
+        }
+    }
+}
