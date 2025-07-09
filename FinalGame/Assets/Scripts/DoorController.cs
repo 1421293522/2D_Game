7d4f3883ec5f0a8f7ee85653c35e2b34966bbc8c
Haining-Li <2893156710@qq.com>
2025-07-09 12:40:06 +0800

    Update

--- FinalGame/Assets/Scripts/DoorController.cs
+++ FinalGame/Assets/Scripts/DoorController.cs
@@ -38,7 +38,7 @@ public class DoorController : MonoBehaviour
     
     void Update()
     {
-        if(isInRange && Input.GetKeyDown(KeyCode.E) && !isOpen)
+        if(isInRange && Input.GetKeyDown(KeyCode.U) && !isOpen)
         {
             StartCoroutine(OpenDoorSequence());
         }
