297a3d2cc34098423c7b5d083309c659bb835fd8
zhoushiyao02200059 <1164641634@qq.com>
2025-07-10 10:12:03 +0800

    hero & enemy prefabs

--- FinalGame/Assets/Scripts/Humanoid/Player/HeroStatus.cs
+++ FinalGame/Assets/Scripts/Humanoid/Player/HeroStatus.cs
@@ -7,12 +7,18 @@ public class HeroStatus : HumanoidStatus
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public override void Die()
+    {
+        mAnimator.SetTrigger("Die");
+        base.Die();
     }
 }
