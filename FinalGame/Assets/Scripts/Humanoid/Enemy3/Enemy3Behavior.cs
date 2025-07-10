09ec43eef880132e6420a56b3729d85466d0bff8
zhoushiyao02200059 <1164641634@qq.com>
2025-07-10 09:50:49 +0800

    enemy prefab

--- FinalGame/Assets/Scripts/Humanoid/Enemy3/Enemy3Behavior.cs
+++ FinalGame/Assets/Scripts/Humanoid/Enemy3/Enemy3Behavior.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy2Behavior : HumanoidBehavior
+public class Enemy3Behavior : HumanoidBehavior
 {
     // Start is called before the first frame update
     void Start()
@@ -15,16 +15,6 @@ public class Enemy2Behavior : HumanoidBehavior
     void Update()
     {
 
-    }
-
-    public void Sleep()
-    {
-        
-    }
-
-    public void Activate()
-    {
-        mAnimator.SetTrigger("Activate");
     }
 
     public override void Idle()
